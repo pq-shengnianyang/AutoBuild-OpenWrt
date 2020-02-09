166c15e854df6beb73c0e800238258df1e7e32e9
pq-shengnianyang <58965430+pq-shengnianyang@users.noreply.github.com>
2020-02-09 13:30:41 +0800

    Update lienol.sh

--- lienol.sh
+++ lienol.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd ~/openwrt &&　echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
